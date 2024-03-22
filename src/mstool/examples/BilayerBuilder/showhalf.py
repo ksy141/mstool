feb7596551b36d619d0c4c1b98b1904e4bb7b29f
ksy141 <kkssy141@gmail.com>
2024-03-22 12:37:27 -0400

    sphereprotein

--- src/mstool/examples/BilayerBuilder/showhalf.py
+++ src/mstool/examples/BilayerBuilder/showhalf.py
@@ -1,5 +1,6 @@
 from chimerax.atomic import all_atoms
 atoms = all_atoms(session)
 bA = atoms.scene_coords[:,1] < 0
+atoms[~bA].residues.atoms.displays = False
 atoms[bA].residues.atoms.displays = True
 
